6c2a3f61ada574cc137e5c182b702d8979c8d121
IlGnome <IlGnome@users.noreply.github.com>
2016-11-27 18:01:19 -0600

    Update foaas.py
    Fixed typo in Fuckee bolding.

--- foaas.py
+++ foaas.py
@@ -33,6 +33,6 @@ def foaas(text, nick, message):
 	Fuckee = text.strip()
 	Fucker = nick
 
-	r = requests.get('http://www.foaas.com/' + str(random.choice(FuckOffList)) + '/' + "\0x2" + Fuckee + "\0x2" + '/' + Fucker, headers=headers)
+	r = requests.get('http://www.foaas.com/' + str(random.choice(FuckOffList)) + '/' + "\x02" + Fuckee + "\x02" + '/' + Fucker, headers=headers)
 	out = r.text
 	message(out)
